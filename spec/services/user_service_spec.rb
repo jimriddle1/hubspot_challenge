e98a1f3639e2c865ed15e239b6d315ca42265693
Jim Riddle <jimriddle0@gmail.com>
2022-10-12 18:55:42 -0500

    wrote test for adding contacts based on vid numbers

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -42,5 +42,33 @@ RSpec.describe 'HubspotUserService' do
       # binding.pry
     end
 
+    it 'adds contacts to the list based on the vid numbers' do
+
+      user1 = User.create(first_name: "Jim", last_name: "Raddle", email: "test@test.com", company: "jims bagels", company_domain: "test.com", phone: "555 555")
+      user2 = User.create(first_name: "Ralph", last_name: "Raddle", email: "test_2@test.com", company: "jims bagels", company_domain: "test.com", phone: "555 555")
+      user3 = User.create(first_name: "Steve", last_name: "Raddle", email: "test_3@test.com", company: "jims bagels", company_domain: "test.com", phone: "555 555")
+      property1 = Property.create(street_address: "test lane", city: "Chicago", state: "IL", postal_code: "12345", status: "deactivated")
+      property2 = Property.create(street_address: "test lane", city: "Naperville", state: "IL", postal_code: "12345", status: "active")
+      property3 = Property.create(street_address: "test lane", city: "Rockford", state: "IL", postal_code: "12345", status: "deactivated")
+
+      user_property1 = UserProperty.create(user: user1, property: property1)
+      user_property2 = UserProperty.create(user: user2, property: property2)
+      user_property3 = UserProperty.create(user: user3, property: property2)
+      user_property4 = UserProperty.create(user: user3, property: property3)
+
+      user_vid_array = HubspotuserService.create_users_with_deactivated_properties
+
+      parsed_json = HubspotuserService.add_contacts_to_list(user_vid_array)
+      data = parsed_json
+      #note - this test will not work because I am do not have a valid list ID.
+      #I tried the list ID off of the readme, but I might be missing something
+      #because that did not work either.
+
+      expect(data).to be_a Hash
+      expect(data).to include :updated, :discarded, :invalidVids
+      expect(data[:udpated]).to be_an(Array)
+
+    end
+
 
 end
