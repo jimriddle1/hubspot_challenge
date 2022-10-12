90bc3bfc286ef7727fb6eef2381ccaf4df375d01
Jim Riddle <jimriddle0@gmail.com>
2022-10-12 15:25:39 -0500

    added properties table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_12_194331) do
+ActiveRecord::Schema.define(version: 2022_10_12_200347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "properties", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "postal_code"
+    t.string "status"
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_10_12_194331) do
     t.text "associated_accounts", default: [], array: true
   end
 
+  add_foreign_key "properties", "users"
 end
--- spec/api/v1/user_validation_spec.rb
+++ spec/api/v1/user_validation_spec.rb
@@ -3,20 +3,15 @@ require 'rails_helper'
 RSpec.describe 'deposit list' do
 
   it 'gives me a list of users that have association with property status deactivated' do
-    # json_response = File.read('spec/fixtures/properties.json')
-    # response_body = JSON.parse(json_response)
-    get "/api/v1/users"
 
-    # @header = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
-    # get "/api/v1/users"
-    # post "/api/v1/users", headers:@header, params: JSON.generate(body)
-    # expect(response).to be_successful
+    user1 = User.new(first_name: "Jim", last_name: "Raddle", email: "test@test.com", company: "jims bagels", company_domain: "test.com", phone: "555 555", associated_accounts: [40,41])
+    binding.pry
 
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    # binding.pry
+
+
   end
 
-  it 'gives me a list of users that have association with property status deactivated' do
+  xit 'gives me a list of users that have association with property status deactivated' do
     # json_response = File.read('spec/fixtures/properties.json')
     # response_body = JSON.parse(json_response)
     post "/api/v1/users"
@@ -27,7 +22,6 @@ RSpec.describe 'deposit list' do
     # expect(response).to be_successful
 
     response_body = JSON.parse(response.body, symbolize_names: true)
-    # binding.pry
   end
 
 
